9b358c754f04de93d9293bdc823f57d06f8b56df
Isaac Jon <isaac.jon72@gmail.com>
2026-06-24 23:33:09 -0400

    db schema planning

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from './Home.jsx';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    }
+  ])
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function Home() {
+  const [count, setCount] = useState(0)
+  
+
+  return (
+    <>
+     <h1>Login</h1>
+    </>
+  )
+}
+
+export default Home
